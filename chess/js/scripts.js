1f2a388ef97506228ba7f57e0dfde19dfd4ca11c
Frugras <frugras@gmail.com>
2016-07-17 18:04:35 +0100

    new lappy

--- chess/js/scripts.js
+++ chess/js/scripts.js
@@ -1,16 +1,23 @@
 $(document).ready(function () {
 	$('#second').hide();
-})
+	$("#input").keyup(function(event){
+	    if(event.keyCode == 13){
+	        $("#submit").click();
+	    }
+	});
+	$('#input').focus();
+});
 
 var you = 1;
 var turn = true;
-var rank = ['a','b','c','d','e','f','g','h'];
-var multipleMsg = 'There are multiple pieces that can make that move';
-var noMoveMsg = "None of your pieces can make that move";
+var rank = ['h','g','f','e','d','c','b','a'];
+// var rank = ['a','b','c','d','e','f','g','h'];
+var multipleMsg = 'There are multiple pieces that can make that (mov)e';
+var no(Mov)eMsg = "None of your pieces can make that (mov)e";
 var white = -1;
 var whiteTurn = true;
-var yourColor = '';
-var theirColor = '';
+var yourColor = 'rgb(255, 255, 255)';
+var theirColor = 'rgb(0, 0, 0)';
 
 // initialise the board with starting positions
 function setUp(color) {
@@ -39,81 +46,82 @@ function setUp(color) {
 		$(row8).html(backRow[8-i]);
 		$(row1).css('color', '#ffffff');
 		$(row2).css('color', '#ffffff');
-		$(for7).css('color', '#000000');
+		$(for7).css('color', '#000000');  
 		$(row8).css('color', '#000000');
-
-		flipBoard();
-
-		//chose which board to display
+ 
+		// chose which board to display
 		if (color == 'white') {
 			$('#first').hide();	
 			$('#second').show();
 		} else {
 			$('#first').show();
 			$('#second').hide();
-		}
-		
+		};
+		flipBoard();
+		$('#input').focus();
+
 	};
 }
 
-// check moves are valid
+// check (mov)es are valid
 function checkSubmit() {
-	var move = $('#input').val().split('');
-	var length = move.length;
-	var validMsg = 'That move is not in valid format';
+	var (mov)e = $('#input').val().split('');
+	var length = (mov)e.length;
+	var validMsg = 'That (mov)e is not in valid format';
 	var validArray = ['a','b','c','d','e','f','g','h','1','2','3','4','5','6','7','8','R','N','B','Q','K','x'];
-	var one = move[0];
-	var two = move[1];
+	var one = (mov)e[0];
+	var two = (mov)e[1];
+	console.log((mov)e)
 	
-	// check move is of correct length
+	// check (mov)e is of correct length
 	if (length < 2 || length > 5) {
-		alert(validMsg);
+		alert(validMsg + '2');
 		return;
 	};
 
 	// check all characters are valid
-	for (var x = 0; x < move.length; x++) {
-		if ($.inArray(move[x], validArray) < 0) {
-			alert(validMsg);
+	for (var x = 0; x < (mov)e.length; x++) {
+		if ($.inArray((mov)e[x], validArray) < 0) {
+			alert(validMsg + '3');
 			return;
 		}
 	};
 
 	// check the target square is valid
-	if (!/[a-h]/.test(move[length-2]) || !/[1-8]/.test(move[length-1])) {
-		alert(validMsg);
+	if (!/[a-h]/.test((mov)e[length-2]) || !/[1-8]/.test(move[length-1])) {
+		alert(validMsg + '4');
 		return;
 	};
 	
 	var pieceCheck = /[RNBKQ]/.test(one);
+	console.log(length)
 	switch (length) {
 		case 2:
 			pawnMove(move);
+			break;
 		case 3:
 			if (pieceCheck) {
-				pieceMove(move);
-			} else if (numOrLet(one)) {
-				pawnAmbigMove(move);
+				pieceMove(move, 0);
 			} else {
-				alert(validMsg);
+				alert(validMsg + '5');
 			};
 			break;
 		case 4:
 			if (numOrLet(one) && 'x' === two) {
 				pawnTake(move);
 			} else if (pieceCheck && 'x' === two) {
-				pieceTake(move);
+				pieceTake(move, 0);
 			} else if (pieceCheck && (numOrLet(two))) {
-				pieceAmbigMove(move);
+				pieceMove(move, 1);
 			} else {
-				alert(validMsg);
+				alert(validMsg + '6');
 			};
 			break;
 		case 5:
 			if (pieceCheck && numOrLet(two) && 'x' === move[2]) {
-				pieceAmbigTake(move)
+				pieceTake(move, 1)
 			} else {
-				alert(validMsg);
+				alert(validMsg + '7');
 			};
 			break;
 	}
@@ -129,18 +137,24 @@ function pawnMove(move) {
 	var x = move[0];
 	x = rank.indexOf(x)+1;
 	var y = Number(move[1]);
-	var from = get(x, y+white);
-	var to = get(x, y);
+	var from = get(x, y+white)[0];
+	var to = get(x, y)[0];
 	console.log(x,y,from,to);
 	// checks a pawn can move one place to the position
 	if (from === 'p' && to === '') {
 		mov(x,y+white,x,y);
 		finished();
 	// check a pawn can move two places to the positiion
-	} else if (get(x, y+2*white) === 'p' && from === '' && to === '') {
+	} else if (get(x, y+2*white)[0] === 'p' && from === '' && to === '') {
 		// make sure piece is on the starting rank, for double move
 		if (turn) {
-			if (y+2*white == 2) {
+			var r;
+			if (whiteTurn) {
+				r = 2;
+			} else {
+				r = 7;
+			}
+			if (y+2*white == r) {
 				mov(x,y+2*white,x,y);
 				finished();
 			} else {
@@ -159,102 +173,354 @@ function pawnMove(move) {
 	};
 };
 
-function pieceMove(move) {
+function pawnTake(move) {
+	var x = move[2];
+	x = rank.indexOf(x)+1;
+	var y = Number(move[3]);
+	var z = rank.indexOf(move[0])+1;;
+	var to = get(x, y);
+	var from = get(z, y+white);
+	// checks a pawn can move one place to the position
+	if (to[0] != '' && to[1] == theirColor && from[0] == 'p' && from[1] == yourColor && Math.abs(z-x) == 1) {
+		mov(x-1, y+white, x, y);
+		finished();
+	} else {
+		alert(moveMsg);
+	};
+};
+
+function pieceMove(move, ambig) {
 	switch (move[0]) {
 		case 'R':
-			rookMove(move);
+			rookMove(move, ambig, 0);
+			break;
 		case 'N':
-			knightMove(move);
+			knightMove(move, ambig, 0);
+			break;
 		case 'B':
+			bishopMove(move, ambig, 0);
+			break;
 		case 'Q':
+			queenMove(move, 0);
+			break;
 		case 'K':
+			kingMove(move, 0);
+			break;
 	}
 };
 
-function pawnAmbigMove(move) {
-
+function pieceTake(move, ambig) {
+	switch (move[0]) {
+		case 'R':
+			rookMove(move, ambig, 1);
+			break;
+		case 'N':
+			knightMove(move, ambig, 1);
+			break;
+		case 'B':
+			bishopMove(move, ambig, 1);
+			break;
+		case 'Q':
+			queenMove(move, 1);
+			break;
+		case 'K':
+			kingMove(move, 1);
+			break;
+	}
 };
 
-function pawnTake(move) {
-
-};
-
-function pieceTake(move) {
-
-};
-
-function pieceAmbigMove(move) {
-
-};
-
-function pieceAmbigTake(move) {
-
-};
-
-function rookMove(move) {
-
-};
-
-function knightMove(move) {
-	var x = move[1];
+function queenMove(move, take) {
+	var x = move[1+take];
 	x = rank.indexOf(x)+1;
-	var y = Number(move[2]);
+	var y = Number(move[2+take]);
 	var to = get(x,y);
-	var positions = [];
-	console.log(x,y)
-	
-	// check all possible starting locations
-	if (get(x-1,y-2) == 'N' && get(x-1,y-2, 'c') == yourColor) {
-		positions.push([x-1,y-2]);			
-	};
-	if (get(x+1,y-2) == 'N' && get(x+1,y-2, 'c') == yourColor) {
-		positions.push([x+1,y-2]);	
-	};
-	if (get(x-1,y+2) == 'N' && get(x-1,y+2, 'c') == yourColor) {
-		positions.push([x-1,y+2]);	
-	};
-	if (get(x+1,y+2) == 'N' && get(x+1,y+2, 'c') == yourColor) {
-		positions.push([x+1,y+2]);	
-	};
-	if (get(x-2,y-1) == 'N' && get(x-2,y-1, 'c') == yourColor) {
-		positions.push([x-2,y-1]);	
-	};
-	if (get(x+2,y-1) == 'N' && get(x+2,y-1, 'c') == yourColor) {
-		positions.push([x+2,y-1]);	
-	};
-	if (get(x-1,y+1) == 'N' && get(x-1,y+1, 'c') == yourColor) {
-		positions.push([x-1,y+1]);	
-	};
-	if (get(x+1,y+1) == 'N' && get(x+1,y+1, 'c') == yourColor) {
-		positions.push([x+1,y+1]);	
-	};
-	console.log(positions);
-	// if multiple/no pieces can make the move then alert, otherwise make the move
-	if (positions.length == 0) {
-		alert(noMoveMsg);
-	} else if (positions.length > 1) {
-		alert(multipleMsg);
+	var pieces = [];
+	var pieces2 = [];
+	var comp;
+	if (take) {
+		comp = (to[0] != '' && to[1] == theirColor);
 	} else {
-		if (get(x,y) == '' || get(x,y,'c') == theirColor) {
-			mov(positions[0][0],positions[0][1],x,y);
-			finished();	
+		comp = to[0] == '';
+	}
+	if (comp) {
+		for (var i = 8; i > 0; i--) {
+			var a = get(x,y+i);
+			if (a[0] != '') {
+				pieces[0]= a;
+			}
+			a = get(x,y-i);
+			if (a[0] != '') {
+				pieces[1]= a;
+			}
+			a = get(x+i,y);
+			if (a[0] != '') {
+				pieces[2]= a;
+			}
+			a = get(x-i,y);
+			if (a[0] != '') {
+				pieces[3]= a;
+			}
+			a = get(x+i,y+i);
+			if (a[0] != '') {
+				pieces[4]= a;
+			}
+			a = get(x+i,y-i);
+			if (a[0] != '') {
+				pieces[5]= a;
+			}
+			a = get(x-i,y+i);
+			if (a[0] != '') {
+				pieces[6]= a;
+			}
+			a = get(x-i,y-i);
+			if (a[0] != '') {
+				pieces[7]= a;
+			}
+		};
+		checkMoves(pieces, 'Q', x, y);
+	} else {
+		alert(noMoveMsg);
+	};
+};
+
+function bishopMove(move, ambig, take) {
+	var x = move[1+ambig+take];
+	x = rank.indexOf(x)+1;
+	var y = Number(move[2+ambig+take]);
+	console.log(x, y);
+	var to = get(x,y);
+	var pieces = [];
+	var pieces2 = [];
+	var comp;
+	if (take) {
+		comp = (to[0] != '' && to[1] == theirColor);
+	} else {
+		comp = to[0] == '';
+	}
+	if (comp) {
+		for (var i = 8; i > 0; i--) {
+			var a = get(x+i,y+i);
+			if (a[0] != '') {
+				pieces[0]= a;
+			}
+			a = get(x+i,y-i);
+			if (a[0] != '') {
+				pieces[1]= a;
+			}
+			a = get(x-i,y+i);
+			if (a[0] != '') {
+				pieces[2]= a;
+			}
+			a = get(x-i,y-i);
+			if (a[0] != '') {
+				pieces[3]= a;
+			}
+		};
+		checkMoves(pieces, 'B', x, y);
+	} else {
+		alert(noMoveMsg);
+	};
+};
+
+function rookMove(move, ambig, take) {
+	var x = move[1+ambig+take];
+	x = rank.indexOf(x)+1;
+	var y = Number(move[2+ambig+take]);
+	var to = get(x,y);
+	var pieces = [];
+	var pieces2 = [];
+	var comp;
+	if (take) {
+		comp = (to[0] != '' && to[1] == theirColor);
+	} else {
+		comp = to[0] == '';
+	}
+	if (comp) {
+		for (var i = 8; i > 0; i--) {
+			var a = get(x,y+i);
+			if (a[0] != '') {
+				pieces[0]= a;
+			}
+			a = get(x,y-i);
+			if (a[0] != '') {
+				pieces[1]= a;
+			}
+			a = get(x+i,y);
+			if (a[0] != '') {
+				pieces[2]= a;
+			}
+			a = get(x-i,y);
+			if (a[0] != '') {
+				pieces[3]= a;
+			}
+		};
+		if (ambig) {
+			checkMoves(pieces, 'R', x, y, move[1]);
 		} else {
-			alert(noMoveMsg);
+			checkMoves(pieces, 'R', x, y);
 		}
 		
+	} else {
+		alert(noMoveMsg);
 	};
 };
 
-// get the letter or color of the piece on given square
-function get(x, y, c) {
-	if (c == 'c') {
-	//return the color
-		return $('table tr:nth-child('+y+') td:nth-child('+x+')').css('color');
+
+function knightMove(move, ambig, take) {
+	console.log(yourColor);
+	var x = move[1+ambig+take];
+	x = rank.indexOf(x)+1;
+	var y = Number(move[2+ambig+take]);
+	var to = get(x,y);
+	var pieces = [];
+	console.log(x,y)
+	var comp;
+	if (take) {
+		comp = (to[0] != '' && to[1] == theirColor);
 	} else {
-	// return the piece
-		return $('table tr:nth-child('+y+') td:nth-child('+x+')').html();
+		comp = to[0] == '';
+	}
+	if (comp) {
+		// check all possible starting locations
+		if (get(x-1,y-2)[0] == 'N' && get(x-1,y-2)[1] == yourColor) {
+			pieces.push(get(x-1,y-2));			
+		};
+		if (get(x+1,y-2)[0] == 'N' && get(x+1,y-2)[1] == yourColor) {
+			pieces.push(get(x+1,y-2));	
+		};
+		if (get(x-1,y+2)[0] == 'N' && get(x-1,y+2)[1] == yourColor) {
+			pieces.push(get(x-1,y+2));	
+		};
+		if (get(x+1,y+2)[0] == 'N' && get(x+1,y+2)[1] == yourColor) {
+			pieces.push(get(x+1,y+2));	
+		};
+		if (get(x-2,y-1)[0] == 'N' && get(x-2,y-1)[1] == yourColor) {
+			pieces.push(get(x-2,y-1));	
+		};
+		if (get(x+2,y-1)[0] == 'N' && get(x+2,y-1)[1] == yourColor) {
+			pieces.push(get(x+2,y-1));	
+		};
+		if (get(x-2,y+1)[0] == 'N' && get(x-2,y+1)[1] == yourColor) {
+			pieces.push(get(x-2,y+1));	
+		};
+		if (get(x+2,y+1)[0] == 'N' && get(x+2,y+1)[1] == yourColor) {
+			pieces.push(get(x+2,y+1));	
+		};
+	} else {
+		alert(noMoveMsg);
+	}
+	console.log(pieces);
+	// if multiple/no pieces can make the move then alert, otherwise make the move
+	if (ambig) {
+		checkMoves(pieces, 'N', x, y, move[1]);
+	} else {
+		checkMoves(pieces, 'N', x, y);
+	}
+
+};
+
+function kingMove(move, take) {
+	console.log(yourColor);
+	var x = move[1+take];
+	x = rank.indexOf(x)+1;
+	var y = Number(move[2+take]);
+	var to = get(x,y);
+	var pieces = [];
+	console.log(x,y)
+	var comp;
+	if (take) {
+		comp = (to[0] != '' && to[1] == theirColor);
+	} else {
+		comp = to[0] == '';
+	}
+	if (comp) {
+		// check all possible starting locations
+		if (get(x,y+1)[0] == 'K' && get(x,y+1)[1] == yourColor) {
+			pieces.push(get(x,y+1));			
+		};
+		if (get(x+1,y+1)[0] == 'K' && get(x+1,y+1)[1] == yourColor) {
+			pieces.push(get(x+1,y+1));	
+		};
+		if (get(x+1,y)[0] == 'K' && get(x+1,y)[1] == yourColor) {
+			pieces.push(get(x+1,y));	
+		};
+		if (get(x+1,y-1)[0] == 'K' && get(x+1,y-1)[1] == yourColor) {
+			pieces.push(get(x+1,y-1));	
+		};
+		if (get(x,y-1)[0] == 'K' && get(x,y-1)[1] == yourColor) {
+			pieces.push(get(x,y-1));
+		};
+		if (get(x-1,y-1)[0] == 'K' && get(x-1,y-1)[1] == yourColor) {
+			pieces.push(get(x-1,y-1));	
+		};
+		if (get(x-1,y)[0] == 'K' && get(x-1,y)[1] == yourColor) {
+			pieces.push(get(x-1,y));	
+		};
+		if (get(x-1,y+1)[0] == 'K' && get(x-1,y+1)[1] == yourColor) {
+			pieces.push(get(x-1,y+1));	
+		};
+	} else {
+		alert(noMoveMsg);
+	};
+	console.log(pieces);
+	// if multiple/no pieces can make the move then alert, otherwise make the move
+	checkMoves(pieces, 'K', x, y);
+};
+
+function checkMoves(pieces, piece, x, y, select) {
+	var pieces2 = [];
+	for (var i = 0; i < pieces.length; i++) {
+		if (pieces[i][0] == piece && pieces[i][1] == yourColor) {
+			pieces2.push(pieces[i]);
+		}
+	}
+	console.log(pieces2)
+	if (pieces2.length == 1) {
+		mov(pieces2[0][2],pieces2[0][3],x,y);
+		finished();
+	} else if (pieces2.length > 1) {
+		if (select != undefined) {
+			console.log(select, rank[pieces2[0][2]-1])
+			if (/[a-h]/.test(select)) {
+				if (select === rank[pieces2[0][2]-1] && select === rank[pieces2[1][2]-1]) {
+					alert("That move could refer to multiple pieces")
+				} else if (select === rank[pieces2[0][2]-1] && select != rank[pieces2[1][2]-1]) {
+					mov(pieces2[0][2],pieces2[0][3],x,y);
+					finished();
+				} else if (select != rank[pieces2[0][2]-1] && select === rank[pieces2[1][2]-1]) {
+					mov(pieces2[1][2],pieces2[1][3],x,y);
+					finished();
+				} else {
+					alert(noMoveMsg);
+				}
+			} else {
+				if (select == pieces2[0][2] && select == pieces2[1][2]) {
+					alert("That move could refer to multiple pieces")
+				} else if (select == pieces2[0][3] && select != pieces2[1][3]) {
+					mov(pieces2[0][2],pieces2[0][3],x,y);
+					finished();
+				} else if (select != pieces2[0][3] && select == pieces2[1][3]) {
+					mov(pieces2[1][2],pieces2[1][3],x,y);
+					finished();
+				} else {
+					alert(noMoveMsg);
+				}
+			}
+		} else {
+			alert(multipleMsg);	
+		}
+	} else {
+		alert(noMoveMsg);
 	};
 }
+
+// get the letter or color of the piece on given square
+function get(x, y) {
+		var q = [$('table tr:nth-child('+y+') td:nth-child('+x+')').html(), $('table tr:nth-child('+y+') td:nth-child('+x+')').css('color'), x, y];
+		console.log(q);
+		return q;
+};
+
 function mov(x, y, i, j) {
 	// find out what piece and color is ont he starting square
 	var piece = $('table tr:nth-child('+y+') td:nth-child('+x+')').html();
@@ -266,7 +532,8 @@ function mov(x, y, i, j) {
 	// put the piece on new tile
 	$('table tr:nth-child('+j+') td:nth-child('+i+')').css('color', color);
 	$('table tr:nth-child('+j+') td:nth-child('+i+')').html(piece);
-}
+	checkCheck(x,y,i,j);
+};
 
 function finished() {
 	// change orientation
@@ -290,12 +557,18 @@ function finished() {
 	} else {
 		yourColor = 'rgb(0, 0, 0)';
 		theirColor = 'rgb(255, 255, 255)';
-	}
-}
+	};
+};
+
 function flip() {
 	$('#first').toggle();
 	$('#second').toggle();
-}
+};
+
+function clearInput() {
+	$('#input').val('');
+};
+
 function flipBoard() {
 	for (var i = 1; i <= 8; i++) {
 		for (var j = 1; j <= 8; j++) {
@@ -305,24 +578,133 @@ function flipBoard() {
 			var l = 9-i;
 			$('table.board2 tr:nth-child('+k+') td:nth-child('+l+')').html(piece);
 			$('table.board2 tr:nth-child('+k+') td:nth-child('+l+')').css('color', color);
-		}
-	}
-}
-/*
-a1
-e5
-ae5
-2f2
-axe3
+		};
+	};
+};
+// /*
+function checkCheck(x,y,i,j) {
+	var pieces = [];
+	var pieces2 = [];
+	for (var x = 0; x < 8; i++) {
+		for (var y = 0; y < 8; y++) {
+			var piece = get(x,y);
+			if (piece[0] == 'K') {
+				if (get(x-1,y-2)[0] == 'N') {
+					pieces.push(get(x-1,y-2));
+				};
+				if (get(x+1,y-2)[0] == 'N') {
+					pieces.push(get(x+1,y-2));
+				};
+				if (get(x-1,y+2)[0] == 'N') {
+					pieces.push(get(x-1,y+2));
+				};
+				if (get(x+1,y+2)[0] == 'N') {
+					pieces.push(get(x+1,y+2));
+				};
+				if (get(x-2,y-1)[0] == 'N') {
+					pieces.push(get(x-2,y-1));
+				};
+				if (get(x+2,y-1)[0] == 'N') {
+					pieces.push(get(x+2,y-1));
+				};
+				if (get(x-2,y+1)[0] == 'N') {
+					pieces.push(get(x-2,y+1));
+				};
+				if (get(x+2,y+1)[0] == 'N') {
+					pieces.push(get(x+2,y+1));
+				};
+				if (piece[1] == yourColor) {
 
-Be3
-Bxe4
-Bba4
-B4b4
-Bbxe4
-B3xf5
-*/
-
+				};
+			};
+			for (var i = 8; i > 0; i--) {
+				var a = get(x,y+i);
+				if (a[0] == 'Q' || a[0] == 'R') {
+					pieces2[0]= a;
+				}
+				a = get(x,y-i);
+				if (a[0] == 'Q' || a[0] == 'R') {
+					pieces2[1]= a;
+				}
+				a = get(x+i,y);
+				if (a[0] == 'Q' || a[0] == 'R') {
+					pieces2[2]= a;
+				}
+				a = get(x-i,y);
+				if (a[0] == 'Q' || a[0] == 'R') {
+					pieces2[3]= a;
+				}
+				a = get(x+i,y+i);
+				if (a[0] == 'Q' || a[0] == 'B') {
+					pieces2[4]= a;
+				}
+				a = get(x+i,y-i);
+				if (a[0] == 'Q' || a[0] == 'B') {
+					pieces2[5]= a;
+				}
+				a = get(x-i,y+i);
+				if (a[0] == 'Q' || a[0] == 'B') {
+					pieces2[6]= a;
+				}
+				a = get(x-i,y-i);
+				if (a[0] == 'Q' || a[0] == 'B') {
+					pieces2[7]= a;
+				}
+			};
+			if (get(x-i,y+white)[0] == 'p') {
+				pieces.push(get(x-i,y+white));
+			};
+			if (get(x+i,y+white)[0] == 'p') {
+				pieces.push(get(x+i,y+white));
+			};
+			if (get(x,y+1)[0] == 'K') {
+				pieces.push(get(x,y+1));			
+			};
+			if (get(x+1,y+1)[0] == 'K') {
+				pieces.push(get(x+1,y+1));	
+			};
+			if (get(x+1,y)[0] == 'K') {
+				pieces.push(get(x+1,y));	
+			};
+			if (get(x+1,y-1)[0] == 'K') {
+				pieces.push(get(x+1,y-1));	
+			};
+			if (get(x,y-1)[0] == 'K') {
+				pieces.push(get(x,y-1));
+			};
+			if (get(x-1,y-1)[0] == 'K') {
+				pieces.push(get(x-1,y-1));	
+			};
+			if (get(x-1,y)[0] == 'K') {
+				pieces.push(get(x-1,y));	
+			};
+			if (get(x-1,y+1)[0] == 'K') {
+				pieces.push(get(x-1,y+1));	
+			};
+			pieces = pieces.concat(pieces2);
+			pieces2 = [];
+			var thisColor;
+			var thisAlert;
+			if (piece[1] == yourColor) {
+				thisColor = theirColor;
+				thisAlert = 'That move will put you in check';
+			} else {
+				thisColor = yourColor;
+				thisAlert = 'Check!';
+			}
+			for (var i = 0; i < pieces.length; i++) {
+				if (pieces[i] != undefined && pieces[i][1] == thisColor) {
+					alert(thisAlert);
+					if(thisColor = yourColor) {
+						// (mov)(i,j,x,y);
+					}
+					break;
+				}
+			}
+		};
+	};
+};
+// */
 
 
 
